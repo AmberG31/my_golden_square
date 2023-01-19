71e6daaa80a58196e7fba7d9d74d56a698447d95
Amber <gynteee@yahoo.co.uk>
2023-01-19 18:22:58 +0000

    Debugged encode.rb

--- intermezzo_debugging_1/encode.rb
+++ intermezzo_debugging_1/encode.rb
@@ -1,22 +1,28 @@
+# copied from another code, need to check the logic with coaches
 def encode(plaintext, key)
-  cipher = key.chars.uniq + (('a'...'z').to_a - key.chars)
+  cipher = key.chars.uniq + (('a'..'z').to_a - key.chars) # changed from exclusive ... to inclusive ..
   ciphertext_chars = plaintext.chars.map do |char|
-    if cipher.find_index(char)
-      (65 + cipher.find_index(char)).chr
-    else
-      "Z"
-    end
+    (65 + cipher.find_index(char)).chr
   end
   return ciphertext_chars.join
 end
 
+
+# not touched
 def decode(ciphertext, key)
-  cipher = key.chars.uniq + (('a'...'z').to_a - key.chars)
+  cipher = key.chars.uniq + (('a'..'z').to_a - key.chars)
   plaintext_chars = ciphertext.chars.map do |char|
-    cipher[65 - char.ord]
+    cipher[char.ord - 65] # minusing 65 from char rather than share from 65
   end
   return plaintext_chars.join
 end
+
+puts encode("theswiftfoxjumpedoverthelazydog", "secretkey")
+puts "EMBAXNKEKSYOVQTBJSWBDEMBPHZGJSL"
+
+puts decode("EMBAXNKEKSYOVQTBJSWBDEMBPHZGJSL", "secretkey")
+puts "theswiftfoxjumpedoverthelazydog"
+
 
 # Intended output:
 #
@@ -26,5 +32,3 @@ end
 # > decode("EMBAXNKEKSYOVQTBJSWBDEMBPHZGJSL", "secretkey")
 # => "theswiftfoxjumpedoverthelazydog"
 
-
-puts encode("theswiftfoxjumpedoverthelazydog", "secretkey")
