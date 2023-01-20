739b1984edbc06b84d2b94cf6304aa96c965aa30
Amber <gynteee@yahoo.co.uk>
2023-01-20 11:21:27 +0000

    Get most common letter debbuged using different solution

--- phase_2_skill_challenges/intermezzo_debugging_1/get_most_common_letter.rb
+++ phase_2_skill_challenges/intermezzo_debugging_1/get_most_common_letter.rb
@@ -1,28 +1,16 @@
 def get_most_common_letter(text)
   counter = Hash.new(0)
   text.chars.each do |char|
-    counter[char] += 1
+    if char.match? /[a-zA-Z]/ # add regular expression to get rid of punctuation and spaces
+      counter[char] += 1
+    end
   end
-  # p counter
-  p counter.to_a.sort_by { |k, v| v }
- #  p counter.to_a.sort_by { |k, v| v }[0][0]
-    counter.to_a.sort_by { |k, v| v }[-2][0] # arrays is sorted descending, we are grabbing the second to last character as the 1st one is a space.
+    counter.to_a.sort_by { |k, v| v }[-1][0] # arrays is sorted descending, we are grabbing the second character.
 end
 
-
-p "Expected 'o' but get #{get_most_common_letter("the roof, the roof, the roof is on fire!")}"
-
+puts get_most_common_letter("the roof, the roof, the roof is on fire!")
 
 # Intended output:
 # 
 # > get_most_common_letter("the roof, the roof, the roof is on fire!")
 # => "o"
-
-
-# def get_most_common_letter(text)
-#   counter = Hash.new(0)
-#   text.chars.each do |char|
-#     counter[char] += 1
-#   end
-#   counter.to_a.sort_by { |k, v| v }[0][0]
-# end
