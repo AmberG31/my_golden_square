4b77c036a0e1528376d43c56b037f8ea15d3ac39
Amber <gynteee@yahoo.co.uk>
2023-01-26 12:24:27 +0000

    letter_counter.rb debugged

--- ph2_skill_challenges/intermezzo_debugging_2/lib/letter_counter.rb
+++ ph2_skill_challenges/intermezzo_debugging_2/lib/letter_counter.rb
@@ -4,24 +4,29 @@ class LetterCounter
   end
 
   def calculate_most_common()
-    counter = Hash.new(1)
+    counter = Hash.new(0)
     most_common = nil
     most_common_count = 1
     @text.chars.each do |char|
       next unless is_letter?(char)
-      counter[char] = (counter[char] || 1) + 1
-      if counter[char] > most_common_count
-        most_common = char
-        most_common_count += counter[char]
+      #binding.irb
+      counter[char] = (counter[char] || 1) +1 # counter[char] = 2
+       # binding.irb
+      if counter[char] > most_common_count # counter[char] is 2 and most_common_count is 1
+        #binding.irb
+        most_common = char # char is i, most common is i
+        most_common_count = counter[char] #most_common_count is 1 and counter[char] is 2. I removed the +
       end
     end
+    #binding.irb
     return [most_common_count, most_common]
   end
 
   private
 
   def is_letter?(letter)
-    return letter =~ /[a-z]/i
+    # binding.irb
+    return letter =~ /[a-z]/i # this is returning "D". After changing line 18 it is returning i as exected 
   end
 end
 
@@ -30,3 +35,6 @@ p counter.calculate_most_common
 
 # Intended output:
 # [2, "i"]
+
+# output 1 = [3, "D"]
+# output 2 = [3, "i"]
