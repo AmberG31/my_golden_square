f0ee83077b3a37e50fc59be5d6c40f313cce7177
Amber <gynteee@yahoo.co.uk>
2023-01-26 14:52:49 +0000

    Finalised debugging

--- ph2_skill_challenges/intermezzo_debugging_2/lib/letter_counter.rb
+++ ph2_skill_challenges/intermezzo_debugging_2/lib/letter_counter.rb
@@ -4,7 +4,7 @@ class LetterCounter
   end
 
   def calculate_most_common()
-    counter = Hash.new(0)
+    counter = Hash.new(0) # changed the argument from 1 to 0
     most_common = nil
     most_common_count = 1
     @text.chars.each do |char|
